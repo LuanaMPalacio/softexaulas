a2e4779d48abf94e8dcacb392762ed2c148bb69a
LuanaMPalacio <luanampalacio@gmail.com>
2025-08-21 15:30:09 -0300

    Primeiro commit

---
+++ aula 01/script.js
@@ -0,0 +1,5 @@
+function mostrarSaudacao() {
+    let nome = "Lua.";
+    const paragrafo = document.getElementById("mensagem"); 
+    paragrafo.textContent = "Olá, " + nome + " parece que você está conseguindo C=";
+}
---
+++ aula 2/script.js
@@ -0,0 +1,8 @@
+function apresentar(){
+    let nome = "Luana";
+    let hobby = "Escrever.";
+    const paragrafo = document.getElementById("mensagem");
+    paragrafo.textContent = "Olá, eu sou " + nome + " e eu gosto muito de " + hobby + " !";
+    
+
+}
