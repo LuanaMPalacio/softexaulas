b62357520287c4f5edb7aadc84d2294e9f8a7eec
Luana Palácio <luanampalacio@gmail.com>
2025-09-11 14:23:11 -0300

    adição

---
+++ aula6_mult/script.js
@@ -0,0 +1,20 @@
+function dobro(x) { //função que calcula o dobro
+    return x * 2;
+}
+
+//escopo principal
+
+document.addEventListener("DOMContentLoaded",() =>{
+    const btn=document.getElementById('btnCalcular');
+    const input =document.getElementById('numero');
+    const resultado=document.getElementById('resultado');
+
+    btn.addEventListener("click", ()=>{
+        const valor= Number(input.value);
+        if(isNaN(valor)) {
+            resultado.textContent="Digite um número válido!";
+            return;
+        }
+        resultado.textContent = `O dobro de ${valor} é ${dobro(valor)}.`;
+    });
+});
